358e99945355030bf0153385a65b2570dbe1c46c
Rajkumar_R <61075470+Rajkumar-21@users.noreply.github.com>
2023-11-13 11:19:45 +0530

    Update url.py with ballons

--- url-shortner/url.py
+++ url-shortner/url.py
@@ -4,7 +4,10 @@ import requests
 import pyshorteners
 
 # Create a title and a text input for the URL
+st.set_page_config(layout="centered")
 st.title("URL Checker and Shortener")
+st.markdown("Enter lengthy url to check status & get short url to use.")
+
 url = st.text_input("Enter a URL to check and shorten:")
 
 # Create a button to submit the URL
@@ -12,16 +15,18 @@ if st.button("Submit"):
     try:
         # Get the response from the URL
         response = requests.get(url)
-        # Display the status code and the final URL
-        st.write(f"Status code: {response.status_code}")
-        st.write(f"Final URL: {response.url}")
+        if response.status_code == 200 :
+            # Display the status code and the final URL
+            st.write(f"Status code: {response.status_code}")
+            st.write(f"Final URL: {response.url}")
 
-        # Initialize the URL shortener
-        s = pyshorteners.Shortener()
+            # Initialize the URL shortener
+            s = pyshorteners.Shortener()
 
-        # Shorten the URL using TinyURL
-        short_url = s.tinyurl.short(url)
-        st.write(f"Shortened URL: {short_url}")
-
+            # Shorten the URL using TinyURL
+            short_url = s.tinyurl.short(url)
+            st.write(f"Shortened URL: {short_url}")
+            st.balloons()
+        
     except Exception as e:
         st.error(f"Invalid URL or network error: {e}")
